638d0eb868dc79b5d8db77cbe0e2c54dcbb683dc
tkfkdqldb1 <tkfkdqldb1@naver.com>
2017-04-26 20:09:43 +0900

    원페어는 숫자 비교이므로 새로운 맵 생성

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -9,7 +9,9 @@ import java.util.Map;
  */
 public class Evaluator {
     public String evaluate(List<Card> cardList) {
+
         Map<Suit, Integer> tempMap = new HashMap<Suit, Integer>();
+        Map<Integer, Integer> numMap = new HashMap<Integer, Integer>(); // 원페어는 숫자가 같아야 하므로 새로운 map 생성
 
         for (Card card : cardList) {
             if (tempMap.containsKey(card.getSuit())) {
@@ -26,6 +28,11 @@ public class Evaluator {
                 return "FLUSH";
             }
         }
+
+
+
+
+
         return "NOTHING";
     }
 }
