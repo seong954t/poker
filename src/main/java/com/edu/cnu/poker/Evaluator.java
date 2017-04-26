7ccaa05cd5935b44f3694d08ae60658e5b1ad3e6
tkfkdqldb1 <tkfkdqldb1@naver.com>
2017-04-26 20:24:16 +0900

    원페어 성공

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -21,11 +21,26 @@ public class Evaluator {
             } else {
                 tempMap.put(card.getSuit(), new Integer(1));
             }
+
+            // 각 card의 rank 값을 비교하여 같은 rank가 몇 번씩 나오는지 count해서 numMap에 rank와 count 저장
+            if (numMap.containsKey(card.getRank())) {
+                Integer count = numMap.get(card.getRank());
+                count = new Integer(count.intValue() + 1);
+                numMap.put(card.getRank(), count);
+            } else {
+                numMap.put(card.getRank(), new Integer(1));
+            }
         }
 
         for (Suit key : tempMap.keySet()) {
             if (tempMap.get(key) == 5) {
                 return "FLUSH";
+            }
+        }
+
+        for (Integer num : numMap.keySet()) {
+            if (numMap.get(num) == 2 ) {
+                return "ONEPAIR";
             }
         }
 
