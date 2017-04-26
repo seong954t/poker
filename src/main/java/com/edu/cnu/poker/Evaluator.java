564f55718431718d6edcbcebefce6d7f5a5c094f
kde0820 <kde0820@naver.com>
2017-04-26 23:40:22 +0900

    refactor

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -11,18 +11,29 @@ import java.util.Map;
 public class Evaluator {
     public String evaluate(List<Card> cardList) {
         Collections.sort(cardList);
-        if (checkfourcard(cardList)) {
+        if (FLUSH(cardList) && checkmountain(cardList)) {
+            return "ROYALSTRAIGHTFLUSH";
+        } else if (FLUSH(cardList)&& cardList.get(0).getRank() == 1 &&STRAIGHT(cardList)) {
+            return "BACKSTRAIGHTFLUSH";
+        } else if (STRAIGHT(cardList) && FLUSH(cardList)) {
+            return "STRAIGHTFLUSH";
+        } else if (checkfourcard(cardList)) {
             return "FOURCARD";
         } else if (FLUSH(cardList)) {
             return "FLUSH";
-        } else if(checkmountain(cardList)){
-            return "MOUNTAIN";
-        }
-        else if (STRAIGHT(cardList)) {
+        } else if (STRAIGHT(cardList)) {
             if (cardList.get(0).getRank() == 1) {
                 return "BACKSTRAIGHT";
             }
             return "STRAIGHT";
+        } else if (checkmountain(cardList)) {
+            return "MOUNTAIN";
+        } else if (checkTriple(cardList)) {
+            return "TRIPLE";
+        } else if (checkTwopair(cardList)) {
+            return "TWOPAIR";
+        } else if (checkOnepair(cardList)) {
+            return "ONEPAIR";
         }
         return "NOTHING";
     }
@@ -31,12 +42,12 @@ public class Evaluator {
         int straight = 0;
         int saveRank = cardList.get(0).getRank() - 1;
         for (Card card : cardList) {
-            if (saveRank+1 == card.getRank()) {
+            if (saveRank + 1 == card.getRank()) {
                 straight++;
             }
             saveRank = card.getRank();
         }
-        if(straight >= 5){
+        if (straight >= 5) {
             return true;
         }
         return false;
@@ -60,6 +71,7 @@ public class Evaluator {
         }
         return false;
     }
+
     public boolean checkfourcard(List<Card> cardList) {
         Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
         for (Card card : cardList) {
@@ -78,20 +90,95 @@ public class Evaluator {
         }
         return false;
     }
+
     public boolean checkmountain(List<Card> cardList) {
         int check = 10;
-        if(cardList.get(0).getRank() == 1 & cardList.get(cardList.size()-1).getRank() == 13){
-            for(Card card : cardList){
-                if(card.getRank() == check){
+        if (cardList.get(0).getRank() == 1 & cardList.get(cardList.size() - 1).getRank() == 13) {
+            for (Card card : cardList) {
+                if (card.getRank() == check) {
                     check++;
                 }
             }
         }
-        if(check == 14){
+        if (check == 14) {
             return true;
         }
         return false;
     }
+
+    public boolean checkTriple(List<Card> cardList) {
+
+        Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
+
+        for (Card card : cardList) {
+            if (numMap.containsKey(card.getRank())) {
+                Integer count = numMap.get(card.getRank());
+                count = new Integer(count.intValue() + 1);
+                numMap.put(card.getRank(), count);
+            } else {
+                numMap.put(card.getRank(), new Integer(1));
+            }
+        }
+
+        for (Integer num : numMap.keySet()) {
+            if (numMap.get(num) == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkTwopair(List<Card> cardList) {
+
+        Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
+
+        Integer onePairCount = 0; // 원페어가 몇 개 존재하는 지 count하는 변수
+
+        // 각 card의 rank 값을 비교하여 같은 rank가 몇 번씩 나오는지 count해서 numMap에 rank와 count 저장
+        for (Card card : cardList) {
+            if (numMap.containsKey(card.getRank())) {
+                Integer count = numMap.get(card.getRank());
+                count = new Integer(count.intValue() + 1);
+                numMap.put(card.getRank(), count);
+            } else {
+                numMap.put(card.getRank(), new Integer(1));
+            }
+        }
+        for (Integer num : numMap.keySet()) {
+            if (numMap.get(num) == 2) {
+                onePairCount += 1;
+            }
+        }
+
+        if (onePairCount == 2) {
+            //원페어가 두 개 존재해야 투페어이다.
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkOnepair(List<Card> cardList) {
+        Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
+        // 각 card의 rank 값을 비교하여 같은 rank가 몇 번씩 나오는지 count해서 numMap에 rank와 count 저장
+        for (Card card : cardList) {
+            if (numMap.containsKey(card.getRank())) {
+                Integer count = numMap.get(card.getRank());
+                count = new Integer(count.intValue() + 1);
+                numMap.put(card.getRank(), count);
+            } else {
+                numMap.put(card.getRank(), new Integer(1));
+            }
+        }
+        for (Integer num : numMap.keySet()) {
+            if (numMap.get(num) == 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
 
 
--- src/test/java/com/edu/cnu/poker/EvaluatorTest.java
+++ src/test/java/com/edu/cnu/poker/EvaluatorTest.java
@@ -72,4 +72,87 @@ public class EvaluatorTest {
 		assertThat(result, is("MOUNTAIN"));
 	}
 
+	@Test
+	public void 같은_숫자_2장이_1쌍이면_원페어다() {
+		Evaluator evaluator = new Evaluator();
+		List<Card> cardList = Arrays.asList(
+				new Card(7,Suit.CLUBS),
+				new Card(2,Suit.HEARTS),
+				new Card(8,Suit.DIAMONDS),
+				new Card(10,Suit.SPADES),
+				new Card(8,Suit.SPADES)
+		);
+		String result = evaluator.evaluate(cardList);
+		assertThat(result, is("ONEPAIR"));
+	}
+
+	@Test
+	public void 스트레이트플러쉬_모양이_같으면_로얄스트레이트플러쉬이다() {
+		Evaluator evaluator = new Evaluator();
+		List<Card> cardList = Arrays.asList(
+				new Card(1,Suit.DIAMONDS),
+				new Card(13,Suit.DIAMONDS),
+				new Card(12,Suit.DIAMONDS),
+				new Card(11,Suit.DIAMONDS),
+				new Card(10,Suit.DIAMONDS)
+		);
+		String result = evaluator.evaluate(cardList);
+		assertThat(result, is("ROYALSTRAIGHTFLUSH"));
+	}
+
+	@Test
+	public void 스트레이트_모양이_같으면_스트레이트플러쉬이다() {
+		Evaluator evaluator = new Evaluator();
+		List<Card> cardList = Arrays.asList(
+				new Card(7,Suit.SPADES),
+				new Card(8,Suit.SPADES),
+				new Card(9,Suit.SPADES),
+				new Card(10,Suit.SPADES),
+				new Card(11,Suit.SPADES)
+		);
+		String result = evaluator.evaluate(cardList);
+		assertThat(result, is("STRAIGHTFLUSH"));
+	}
+
+	@Test
+	public void 백스트레이트_모양이_같으면_백스트레이트플러쉬이다() {
+		Evaluator evaluator = new Evaluator();
+		List<Card> cardList = Arrays.asList(
+				new Card(5,Suit.CLUBS),
+				new Card(3,Suit.CLUBS),
+				new Card(1,Suit.CLUBS),
+				new Card(4,Suit.CLUBS),
+				new Card(2,Suit.CLUBS)
+		);
+		String result = evaluator.evaluate(cardList);
+		assertThat(result, is("BACKSTRAIGHTFLUSH"));
+	}
+
+	@Test
+	public void 같은_숫자_2장이_2쌍이면_투페어다() {
+		Evaluator evaluator = new Evaluator();
+		List<Card> cardList = Arrays.asList(
+				new Card(2,Suit.CLUBS),
+				new Card(2,Suit.HEARTS),
+				new Card(3,Suit.DIAMONDS),
+				new Card(4,Suit.SPADES),
+				new Card(3,Suit.SPADES)
+		);
+		String result = evaluator.evaluate(cardList);
+		assertThat(result, is("TWOPAIR"));
+	}
+
+	@Test
+	public void 같은_숫자가_3장이면_트리플이다() {
+		Evaluator evaluator = new Evaluator();
+		List<Card> cardList = Arrays.asList(
+				new Card(3, Suit.CLUBS),
+				new Card(2, Suit.HEARTS),
+				new Card(7, Suit.DIAMONDS),
+				new Card(3, Suit.SPADES),
+				new Card(3, Suit.SPADES)
+		);
+		String result = evaluator.evaluate(cardList);
+		assertThat(result, is("TRIPLE"));
+	}
 }
